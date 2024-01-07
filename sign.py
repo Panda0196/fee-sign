27d18e4267f2867847b9c5d3862e58637e89a921
Panda0196 <mike.wazowski0196@gmail.com>
2024-01-07 10:20:03 -0800

    release: v2

--- sign.py
+++ sign.py
@@ -1,6 +1,7 @@
 # https://web3py.readthedocs.io/en/latest/web3.eth.account.html#verify-a-message-with-ecrecover-in-solidity
 
 import os
+import time
 from eth_account import Account
 from eth_account.signers.local import LocalAccount
 from web3 import Web3, EthereumTesterProvider
@@ -8,36 +9,44 @@ from web3.middleware import construct_sign_and_send_raw_middleware
 
 w3 = Web3(EthereumTesterProvider())
 
-# private_key = os.environ.get("PRIVATE_KEY")
-# assert private_key is not None, "You must set PRIVATE_KEY environment variable"
-# assert private_key.startswith("0x"), "Private key must start with 0x hex prefix"
 
-# account: LocalAccount = Account.from_key(private_key)
-# w3.middleware_onion.add(construct_sign_and_send_raw_middleware(account))
+# Get private key
+private_key = os.environ.get("PRIVATE_KEY")
+if private_key is None:
+    private_key = input("\nInput private key: ")
+assert private_key.startswith("0x"), "\nPrivate key must start with 0x hex prefix"
 
-# print(f"Your hot wallet address is {account.address}")
+account: LocalAccount = Account.from_key(private_key)
+w3.middleware_onion.add(construct_sign_and_send_raw_middleware(account))
+print(f"\nWallet address: {account.address}")
 
-# Now you can use web3.eth.send_transaction(), Contract.functions.xxx.transact() functions
-# with your local private key through middleware and you no longer get the error
-# "ValueError: The method eth_sendTransaction does not exist/is not available
 
+# Input parameters
+print("\nfunction feeCheck(address token, bytes calldata signature, uint transferFee, uint bridgeFee, uint amount)\n")
+token = input("address token: ")
+assert token.startswith("0x"), "\nToken address must start with 0x hex prefix"
+assert len(token) == 42, "\nToken address length mush be 42"
+transferFee = int(input("uint transferFee: "))
+bridgeFee = int(input("uint bridgeFee: "))
+amount = int(input("uint amount: "))
 
 
 # 1. Sign a Message
 from eth_account.messages import encode_defunct
+hash = Web3.solidity_keccak(["address", "uint256", "uint256", "uint256", "uint256"], [token, int(time.time()/1800), transferFee, bridgeFee, amount])
+# print("\nHash message:", Web3.to_hex(hash))
 
-msg = "I♥SF"
-private_key = b"\xb2\\}\xb3\x1f\xee\xd9\x12''\xbf\t9\xdcv\x9a\x96VK-\xe4\xc4rm\x03[6\xec\xf1\xe5\xb3d"
-message = encode_defunct(text=msg)
-signed_message = w3.eth.account.sign_message(message, private_key=private_key)
-print("\n\nSigned message:", signed_message)
+# msg_hash = defunct_hash_message(hexstr=hash.hex(hash))
+# signed_message = w3.eth.account.signHash(msg_hash, private_key=private_key)
 
+msg_hash = encode_defunct(hexstr=Web3.to_hex(hash))
+signed_message = w3.eth.account.sign_message(msg_hash, private_key=private_key)
+print("\nSigned message:", signed_message)
 
 
 # 2. Verify a Message
-message = encode_defunct(text="I♥SF")
-vecover_message = w3.eth.account.recover_message(message, signature=signed_message.signature)
-print("\n\nRecover message:", vecover_message)
+vecover_message = w3.eth.account.recover_message(msg_hash, signature=signed_message.signature)
+print("\nMessage signer:", vecover_message)
 
 
 
@@ -46,13 +55,13 @@ print("\n\nRecover message:", vecover_message)
 # ecrecover in Solidity expects v as a native uint8, but r and s as left-padded bytes32
 # Remix / web3.js expect r and s to be encoded to hex
 # This convenience method will do the pad & hex for us:
-def to_32byte_hex(val):
-    return Web3.to_hex(Web3.to_bytes(val).rjust(32, b'\0'))
+# def to_32byte_hex(val):
+#     return Web3.to_hex(Web3.to_bytes(val).rjust(32, b'\0'))
 
-ec_recover_args = (msghash, v, r, s) = (
-    Web3.to_hex(signed_message.messageHash),
-    signed_message.v,
-    to_32byte_hex(signed_message.r),
-    to_32byte_hex(signed_message.s),
-)
-print("\n\nec_recover_args:", ec_recover_args)
+# ec_recover_args = (msghash, v, r, s) = (
+#     Web3.to_hex(signed_message.messageHash),
+#     signed_message.v,
+#     to_32byte_hex(signed_message.r),
+#     to_32byte_hex(signed_message.s),
+# )
+# print("\nec_recover_args:", ec_recover_args)
--- verify.sol
+++ verify.sol
@@ -1,10 +1,69 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.22;
+// Deployed at https://goerli.etherscan.io/address/0x6243b6eb4d56e0969f4d5e72c020f7c024b0b52b#code#L35
 
-contract Recover {
-  function ecr (bytes32 msgh, uint8 v, bytes32 r, bytes32 s) public pure
-  returns (address sender) {
-    return ecrecover(msgh, v, r, s);
-  }
+pragma solidity ^0.8.6;
+
+import "hardhat/console.sol";
+
+function ecdsaRecover(bytes32 messageHash, bytes memory signature) pure returns(address) {
+    bytes32 r;
+    bytes32 s;
+    uint8 v;
+    address ret;
+
+    assembly {
+        r := mload(add(signature, 32))
+        s := mload(add(signature, 64))
+        v := byte(0, mload(add(signature, 96)))
+        if lt(v, 27) {v := add(v, 27)}
+    }
+
+    ret = ecrecover(messageHash, v, r, s);
+
+    console.log("ecrecover input");
+    console.logBytes32(messageHash);
+    console.log(v);
+    console.logBytes32(r);
+    console.logBytes32(s);
+    console.log("address:", ret);
+
+    return ret;
+}
+
+/** 
+ * @title test sign.py
+ * @dev https://etherscan.io/address/0xc889b800ddaa43d828365a75194A9BAf21445111#code
+ */
+contract Verif {
+
+    // Signature contains timestamp divided by SIGNATURE_FEE_TIMESTAMP; SIGNATURE_FEE_TIMESTAMP should be the same on relay;
+    uint private constant SIGNATURE_FEE_TIMESTAMP = 1800;  // 30 min
+
+    // Signature will be valid for `SIGNATURE_FEE_TIMESTAMP` * `signatureFeeCheckNumber` seconds after creation
+    uint internal signatureFeeCheckNumber = 3;
+
+    // encode message with received values and current timestamp;
+    // check that signature is same message signed by address with RELAY_ROLE;
+    // make `signatureFeeCheckNumber` attempts, each time decrementing timestampEpoch (workaround for old signature)
+    function feeCheck(address token, bytes calldata signature, uint transferFee, uint bridgeFee, uint amount) public view returns(address) {
+        bytes32 messageHash;
+        address signer;
+        uint timestampEpoch = block.timestamp / SIGNATURE_FEE_TIMESTAMP;
+
+        for (uint i = 0; i < signatureFeeCheckNumber; i++) {
+            messageHash = keccak256(abi.encodePacked(
+                    "\x19Ethereum Signed Message:\n32",
+                    keccak256(abi.encodePacked(token, timestampEpoch, transferFee, bridgeFee, amount))
+                ));
+
+            signer = ecdsaRecover(messageHash, signature);
+            //if (hasRole(FEE_PROVIDER_ROLE, signer))
+                //return;
+            if (token == signer)
+                return signer;
+            timestampEpoch--;
+        }
+        revert("Signature check failed");
+    }
 }
